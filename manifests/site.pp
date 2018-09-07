fa69565ec4a9eecd2a8da6998db8421892856931
root <root@laoyouyu4.mylabserver.com>
2018-09-07 21:28:13 +1000

    disbale node reg in site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -24,11 +24,11 @@ File { backup => false }
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
-node /^laoyouyu/ {
-  notify { "Notify":
-    message=>"A message from common : ${facts['fqdn']}.",
-  }
-}
+#node /^laoyouyu/ {
+#  notify { "Notify":
+#    message=>"A message from common : ${facts['fqdn']}.",
+#  }
+#}
 
 node default {
   # This is where you can declare classes for all nodes.
